b42a6b896f32d4a18623b65f98e947cb6254d1e7
ui-adiii <beaheroinyourlife@gmail.com>
2025-07-29 11:43:22 +0530

    adding credential

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
+   credentials: true,
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
