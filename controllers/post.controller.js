50df73c5af2b948b5a7a1ef95fa43c8fc25560bf
Aditya Mallick <147989750+Ui-Adiii@users.noreply.github.com>
2025-09-16 17:27:18 +0530

    Update post.controller.js

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -3,7 +3,7 @@ import { v2 as cloudinary } from "cloudinary";
 
 const createPost = async (req, res) => {
   try {
-    if (!req.user) {
+    if (!req.user.isAdmin) {
       return res.status(403).json({
         success: false,
         message: "You are not allowed for create a post",
